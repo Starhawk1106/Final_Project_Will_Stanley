5fd6f69d6abdaa690c648003124f3cf6ddb1a4d4
Will Stanley <whs17a@acu.edu>
2020-11-21 15:11:32 -0600

    UI Works

---
+++ Final_Project_Will_Stanley/ARFTableViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ARFTableViewController.swift
+//  Final_Project_Will_Stanley
+//
+//  Created by William Stanley on 11/21/20.
+//
+
+import UIKit
+
+class ARFTableViewController: UITableViewController {
+    
+    //DELET LATER
+    var outerArray = ["Dine-In Restaurants", "Take Out Restaurants", "Fast Food Restaurants"]
+    var innerArray1 = ["Restaurant 1", "Restaurant 2", "Restaurant 3"]
+    var innerArray2 = ["Restaurant 1", "Restaurant 2", "Restaurant 3", "Restaurant 4"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "ARF"
+        
+        // 1
+        let nav = self.navigationController?.navigationBar
+         
+        // 2
+        nav?.barStyle = UIBarStyle.black
+        nav?.tintColor = UIColor.init(red: 138/255, green: 43/255, blue: 226/255, alpha: 1)
+         
+        // 3
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 60))
+        imageView.contentMode = .scaleAspectFit
+             
+        // 4
+        let image = UIImage(named: "ARFLogo")
+        imageView.image = image
+             
+        // 5
+        navigationItem.titleView = imageView
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return outerArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if (section == 0) {
+            return innerArray1.count
+        } else if (section == 1) {
+            return innerArray2.count
+        } else {
+            return 1
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return outerArray[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mediaCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = "put some stuff here"
+
+        return cell
+    }
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
